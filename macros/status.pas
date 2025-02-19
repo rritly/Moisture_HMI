87df14087fd836e1e756da001772e669f3ad7c89
Roman Istomin <ra.istomin@yandex.ru>
2025-02-19 23:21:17 +0300

    update

--- macros/status.pas
+++ macros/status.pas
@@ -1,5 +1,6 @@
 
 macro_command main()
+
 short status1 = 0
 short status2 = 0
 bool waiting1 = false, initia1 = false, run1 = false, faultSP1 = false, onSW1 = false
@@ -16,7 +17,7 @@ GetData(allStatus, "Local HMI", LW, 92, 1)
 DELAY(40)
 
 if allStatus == 1 then
-	emergency = true
+emergency = true
 else
 end if
 
@@ -24,37 +25,37 @@ generator0 = TOFON0(emergency, 6)
 
 select case status1
 case 0
- 	waiting1 = true
+waiting1 = true
 break
 case 1 
- 	onSW1 = true
+onSW1 = true
 break
 case 2
- 	initia1 = true
+initia1 = true
 break
 case 3 
-	run1 = true
+run1 = true
 break
 case 4
-	faultSP1 = true
+faultSP1 = true
 break
 end select
 
 select case status2
 case 0
-	waiting2 = true
+waiting2 = true
 break
 case 1 
-	onSW2 = true
+onSW2 = true
 break
 case 2
- 	initia2 = true
+initia2 = true
 break
 case 3 
- 	run2 = true
+run2 = true
 break
 case 4
- 	faultSP2 = true
+faultSP2 = true
 break
 end select
 
@@ -63,13 +64,11 @@ SetData(onSW1, "Local HMI", LB, 61, 1)
 SetData(initia1, "Local HMI", LB, 62, 1)
 SetData(run1, "Local HMI", LB, 63, 1)
 SetData(faultSP1, "Local HMI", LB, 64, 1)
-
 SetData(waiting2, "Local HMI", LB, 65, 1)
 SetData(onSW2, "Local HMI", LB, 66, 1)
 SetData(initia2, "Local HMI", LB, 67, 1)
 SetData(run2, "Local HMI", LB, 68, 1)
 SetData(faultSP2, "Local HMI", LB, 69, 1)
-
 SetData(generator0, "Local HMI", LB, 70, 1)
 
 end macro_command
