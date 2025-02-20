bd41a02730cdfee4172c0f532e35d5aa589062ff
Roman Istomin <ra.istomin@yandex.ru>
2025-02-20 18:28:16 +0300

    update

--- macros/GetSet1Plc.pas
+++
@@ -1,9 +0,0 @@
-// 62 ms
-macro_command main()
-
-int currDistance1Down = 0
-
-GetDataEx(currDistance1Down, "Siemens S7-1200/S7-1500", "PLC.Blocks.READ.lower.currDistance", 1)
-SetData(currDistance1Down, "Local HMI", LW, 90, 1)
-
-end macro_command
